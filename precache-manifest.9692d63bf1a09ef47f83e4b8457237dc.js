b5fd6e9376f7d3990221dc54c5006a71105dab06
Sasha Zayets <sachazaets@ukr.net>
2019-12-08 20:24:05 +0200

    Updates

---
+++ precache-manifest.9692d63bf1a09ef47f83e4b8457237dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a2dce0d21eafcc78cc606e591a76d99",
+    "url": "/news-portal/index.html"
+  },
+  {
+    "revision": "6898af1404cc3444b279",
+    "url": "/news-portal/static/css/main.1dd764e4.chunk.css"
+  },
+  {
+    "revision": "524d96bb8aaf2f2c4ab0",
+    "url": "/news-portal/static/js/2.3b240c8b.chunk.js"
+  },
+  {
+    "revision": "6898af1404cc3444b279",
+    "url": "/news-portal/static/js/main.eb511f95.chunk.js"
+  },
+  {
+    "revision": "e7e66df25c40e41d87ad",
+    "url": "/news-portal/static/js/runtime-main.550f8154.js"
+  }
+]);
